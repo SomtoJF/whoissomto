bd17fa162aec5c4a5fff3882df864a73a66dfd89
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-20 00:39:31 +0100

    Initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import "./Hero.styles.sass";
+
+export default function Hero() {
+	return (
+		<header id="hero-section">
+			<h1>
+				FRANCIS <br /> SOMTOCHUKWU
+			</h1>
+			<p>FULLSTACK SOFTWARE DEVELOPER</p>
+			<p>EST. TWENTY YEARS AGO</p>
+		</header>
+	);
+}
