e40a2081948b60d3693e8b1089a1cfc5d86cdb94
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-20 01:07:45 +0100

    Import about hero and experience sections

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,3 +1,14 @@
+import About from "../components/About";
+import Experience from "../components/Experience";
+import Hero from "../components/Hero";
+import "./Portfolio.styles.sass";
+
 export default function Portfolio() {
-	return <div>Portfolio</div>;
+	return (
+		<div id="portfolio-page">
+			<Hero />
+			<About />
+			<Experience />
+		</div>
+	);
 }
