e68764faa08230114c5cb22fcf7f9fac4e274e5d
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-22 23:20:08 +0100

    Redesign and import new hero

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,14 +1,10 @@
-import About from "../components/About";
-import Experience from "../components/Experience";
-import Hero from "../components/Hero";
+import Hero from "../components/Hero/Hero";
 import "./Portfolio.styles.sass";
 
 export default function Portfolio() {
 	return (
 		<div id="portfolio-page">
 			<Hero />
-			<About />
-			<Experience />
 		</div>
 	);
 }
