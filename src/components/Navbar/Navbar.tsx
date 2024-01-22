2ff15d69711c7113229b24504703ec5b3a3f2c1b
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-22 20:30:25 +0100

    Initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.styles.sass";
+
+export default function Navbar() {
+	return (
+		<nav id="navbar">
+			<ul>
+				<li>WHOAMI?</li>
+				<li>EXPERIENCE</li>
+				<li>PROJECTS</li>
+				<li>BLOG</li>
+			</ul>
+		</nav>
+	);
+}
