873146bf50162a1fdae08f25532726c8daaf9df3
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-24 00:53:36 +0100

    Implemement smooth scroll with lenis

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Layout() {
 	return (
-		<>
+		<ReactLenis root options={{ lerp: 0.07, duration: 1.5, smoothTouch: true }}>
 			<Navbar />
 			<Outlet />
-		</>
+		</ReactLenis>
 	);
 }
