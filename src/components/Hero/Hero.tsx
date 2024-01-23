509fb8d24c44d34fd77ab56b6e69bd2baafc9b0c
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-23 23:50:28 +0100

    Declare new ref for the hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,21 +1,23 @@
 import "./Hero.styles.sass";
 import Marquee from "react-fast-marquee";
 import ScrollNav from "./ScrollNav";
+import { useRef } from "react";
 
 export default function Hero() {
+	const heroRef = useRef<HTMLElement>(null);
 	return (
-		<section id="hero">
+		<section id="hero" ref={heroRef}>
 			<div>
 				<ul id="welcome-message">
 					<li>WELCOME TO MY WORLD</li>
 				</ul>
-				<p>
+				<p id="intro">
 					Final year Software Engineering Student/
 					<em>Fullstack Software Developer</em> on a mission to build simple
 					solutions to <em>complex</em> real-world problems.
 				</p>
 			</div>
-			<ScrollNav />
+			<ScrollNav heroRef={heroRef} />
 			<div id="marquee">
 				<Marquee pauseOnHover speed={100} delay={2} autoFill>
 					<div>FRANCIS SOMTOCHUKWU</div>
