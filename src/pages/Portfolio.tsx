1c7c94117f20b2117e78613521796c0a38ebbe3a
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-24 23:05:21 +0100

    disable animated cursor on mobile devices

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -5,20 +5,22 @@ import "./Portfolio.styles.sass";
 export default function Portfolio() {
 	return (
 		<div id="portfolio-page">
-			<AnimatedCursor
-				color="0,0,0,0"
-				innerSize={5}
-				outerSize={5}
-				innerScale={1}
-				outerScale={9}
-				outerAlpha={0}
-				outerStyle={{
-					background: "white",
-					mixBlendMode: "exclusion",
-				}}
-				trailingSpeed={1}
-				showSystemCursor={true}
-			/>
+			{window.innerWidth > 900 && (
+				<AnimatedCursor
+					color="0,0,0,0"
+					innerSize={5}
+					outerSize={5}
+					innerScale={1}
+					outerScale={9}
+					outerAlpha={0}
+					outerStyle={{
+						background: "white",
+						mixBlendMode: "exclusion",
+					}}
+					trailingSpeed={1}
+					showSystemCursor={true}
+				/>
+			)}
 			<Hero />
 		</div>
 	);
