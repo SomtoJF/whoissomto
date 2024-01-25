01424ecb6d0a46a29341c0b79fa9472eebe4c9cd
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-25 01:05:27 +0100

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,35 @@
+import "./About.styles.sass";
+
+export default function About() {
+	return (
+		<div id="about">
+			<h1>Who is Francis Somtochukwu?</h1>
+			<p>
+				Lorem ipsum dolors sit amet, cons ectetur adipisci elit, sed do eiusmod
+				tempor inc ididunt ut labores et dolore ercit ati on ull amco laboris
+				nisi ut aliqui. Dui aute irur tu dolo end erit in volup tate velit ese
+				cilu ei dolore eu fugiat. Eam et lucilius delicat. At fabulas inimicus
+				cum, per quise an. Id per ferri rum mediocrem, mea ne graeci dolorem
+				vituperata, habeo ullum disputando an sit. Veritus um no qui, at usu
+				quod at inimicuse psum dolor si dicam labore. Meas iusto oporter and.
+				Ius vitae parte repudi andae, sed an harum simul interpretaris. Cu vix
+				bore euis modus dolorum. Has nore ame dolor, pro reque.
+			</p>
+			<div id="social-links">
+				<a
+					href="https://www.linkedin.com/in/somtochukwu-francis-b8a236239"
+					target="_blank"
+				>
+					LINKEDIN
+				</a>
+				<a href="https://twitter.com/somtofrancis3" target="_blank">
+					X/TWITTER
+				</a>
+				<a href="https://github.com/SomtoJF" target="_blank">
+					GITHUB
+				</a>
+			</div>
+			<figure></figure>
+		</div>
+	);
+}
