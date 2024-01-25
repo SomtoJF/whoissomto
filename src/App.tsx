6893eb92d631852e852704b71e6f3ff154f704ee
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-25 01:06:06 +0100

    Import about page and create route for it

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import "./App.sass";
 import Layout from "./Layout";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
-		children: [{ path: "/", element: <Portfolio /> }],
+		children: [
+			{ path: "/", element: <Portfolio /> },
+			{ path: "/about", element: <About /> },
+		],
 	},
 ]);
 
