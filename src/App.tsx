40bbc4fb6a613007b7cfe414adb7d38c358007bc
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-25 11:20:32 +0100

    Migrate from createBrowserRouter to normal routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import "./App.sass";
 import Layout from "./Layout";
 import About from "./pages/About";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{ path: "/", element: <Portfolio /> },
-			{ path: "/about", element: <About /> },
-		],
-	},
-]);
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+        </Route>
+    )
+);
 
 function App() {
-	return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
