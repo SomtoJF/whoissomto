c061fab080777e815c2805b19b5b6a6dbc7e9aa8
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-25 14:35:47 +0100

    Add route for projects

--- src/AnimatedRoutes.tsx
+++ src/AnimatedRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import { AnimatePresence } from "framer-motion";
+import Projects from "./pages/Projects";
 
 const AnimatedRoutes = () => {
 	const location = useLocation();
@@ -12,6 +13,7 @@ const AnimatedRoutes = () => {
 				<Route path="/" element={<Layout />}>
 					<Route path="/" element={<Portfolio />} />
 					<Route path="/about" element={<About />} />
+					<Route path="/projects" element={<Projects />} />
 				</Route>
 			</Routes>
 		</AnimatePresence>
