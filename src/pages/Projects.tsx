6ae69f5af56ecb6342a248fb7c8dfa528c9a3aed
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-25 23:44:25 +0100

    Link to github

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,4 @@
+import { ArrowRightOutlined } from "@ant-design/icons";
 import AnimatedPage from "../components/AnimatedPage/AnimatedPage";
 import Swiper from "../components/Projects/Swiper";
 import "./Projects.styles.sass";
@@ -6,6 +7,9 @@ export default function Projects() {
 	return (
 		<AnimatedPage id="projects">
 			<Swiper />
+			<a href="https://github.com/SomtoJF/" target="_blank" id="link-to-gh">
+				MORE ON MY GITHUB <ArrowRightOutlined />
+			</a>
 		</AnimatedPage>
 	);
 }
