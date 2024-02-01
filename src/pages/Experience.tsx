47cc817329cea438cf3524c535571214bfc9dd11
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-02 00:40:04 +0100

    Import new education component

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -3,12 +3,14 @@ import AnimatedPage from "../components/AnimatedPage/AnimatedPage";
 import "./Experience.styles.sass";
 import { Link } from "react-router-dom";
 import ExperienceComponent from "../components/Experience&Education/Experience";
+import EducationComponent from "../components/Experience&Education/Education";
 
 export default function Experience() {
 	return (
 		<AnimatedPage id="experience-education">
 			<h1>Experience & Education</h1>
 			<ExperienceComponent />
+			<EducationComponent />
 			<Link to={"/projects"} id="link-to-projects">
 				VIEW MY PROJECTS <ArrowRightOutlined />
 			</Link>
