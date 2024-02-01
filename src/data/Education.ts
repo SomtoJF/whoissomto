55a952ca2b62016e80762004ff4e101531adc27f
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-02 00:37:30 +0100

    Initial commit

---
+++ src/data/Education.ts
@@ -0,0 +1,16 @@
+import { Education } from "../Types/Education";
+
+const myEducation: Education[] = [
+	{
+		program: "Bsc Software Engineering",
+		school: "Babcock Univerity",
+		department: "Software Engineering",
+		period: {
+			start: 2020,
+			end: 2024,
+		},
+		location: "Ilishan-Remo, Ogun State.",
+	},
+];
+
+export default myEducation;
